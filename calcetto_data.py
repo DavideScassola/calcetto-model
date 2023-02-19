57cf3d145eab1e89093e7a027cfd1a2ee3a68b5b
Davide Scassola <davide.scassola1@gmail.com>
2023-02-20 00:35:01 +0100

    first working version

--- calcetto_data.py
+++ calcetto_data.py
@@ -11,46 +11,46 @@ WINNER = "risultato"
 
 def from_notion_csv(file: str) -> pd.DataFrame:
     df = pd.read_csv(file)
-    to_list = lambda c: c.str.split(", ")
-    df[TEAM_A] = to_list(df[TEAM_A])
-    df[TEAM_B] = to_list(df[TEAM_B])
+    to_set = lambda c: c.str.split(", ").apply(set)
+    df[TEAM_A] = to_set(df[TEAM_A])
+    df[TEAM_B] = to_set(df[TEAM_B])
     useful_columns = [TEAM_A, TEAM_B, GOALS_A, GOALS_B, WINNER]
     return df[useful_columns]
 
 
 @dataclass
 class Match:
-    team_a: list
-    team_b: list
+    team_a: set
+    team_b: set
     goals_a: int
     goals_b: int
 
     def total_goals(self) -> int:
-        return self.goals_a + self.team_b
+        return self.goals_a + self.goals_b
 
 
 class CalcettoData:
     def __init__(self, file: str) -> None:
         self.df = from_notion_csv(file)
-        self.build_players_list(self.df)
-        self.build_matches_list(self.df)
+        self.__build_players_list(self.df)
+        self.__build_matches_list(self.df)
 
-    def build_players_list(self, df: pd.DataFrame):
-        self.players = set().union(*df[TEAM_A], *df[TEAM_B])
+    def __build_players_list(self, df: pd.DataFrame):
+        self.players = sorted(list(set().union(*df[TEAM_A], *df[TEAM_B])))
 
-    def build_matches_list(self, df: pd.DataFrame):
+    def __build_matches_list(self, df: pd.DataFrame):
         def from_df_row(row: dict):
             return Match(
                 team_a=row[TEAM_A],
                 team_b=row[TEAM_B],
                 goals_a=row[GOALS_A],
-                goals_b=row[TEAM_B],
+                goals_b=row[GOALS_B],
             )
 
         self.matches = [from_df_row(r) for r in df.iloc]
 
-    def players(self) -> list:
+    def get_players(self) -> list:
         return self.players
 
-    def matches(self) -> List[Match]:
-        pass
+    def get_matches(self) -> List[Match]:
+        return self.matches
--- main.py
+++ main.py
@@ -1,39 +1,100 @@
 import pyro.distributions as dist
 import pyro
 import torch
+from calcetto_data import CalcettoData
+from pyro.infer.autoguide.guides import AutoMultivariateNormal
+from pyro.optim import Adam
+from pyro.infer import SVI, Trace_ELBO
+from tqdm import tqdm
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
 
-PRIOR = {"mu_log2_skill": -0.29, "sigma_log2_skill": 0.1}
+# PRIOR = {"mu_log2_skill": -0.29, "sigma_log2_skill": 0.1}
+PRIOR = {"mu_log2_skill": 0.0, "sigma_log2_skill": 0.5}
 
 
-def model(data):
+def model(data: CalcettoData):
     mu_pior = torch.tensor(PRIOR["mu_log2_skill"])
     sigma_pior = torch.tensor(PRIOR["sigma_log2_skill"])
 
-    latent_skill[data.reference_player] = 1.0  # TODO: maybe I have to put an array
-    latent_skill = {
+    # latent_log2_skill[data.reference_player] = 1.0  # TODO: maybe I have to put an array
+    latent_log2_skill = {
         p: pyro.sample(
-            f"latent_skill_{p}", dist.LogNormal(loc=mu_pior, scale=sigma_pior)
+            f"latent_log2_skill_{p}", dist.Normal(loc=mu_pior, scale=sigma_pior)
         )
-        for p in data.players(include_reference_player=False)
+        for p in data.get_players()
+        # for p in data.players(include_reference_player=False)
     }
 
-    for m, i in enumerate(data.matches()):
-        latent_skill_team_a = sum(latent_skill[p] for p in m.team_a())
-        latent_skill_team_b = sum(latent_skill[p] for p in m.team_b())
+    latent_skill = {name: torch.exp2(ls) for name, ls in latent_log2_skill.items()}
+
+    for i, m in enumerate(data.get_matches()):
+        latent_skill_team_a = sum(latent_skill[p] for p in m.team_a)
+        latent_skill_team_b = sum(latent_skill[p] for p in m.team_b)
         prob_A = latent_skill_team_a / (latent_skill_team_a + latent_skill_team_b)
         pyro.sample(
-            f"match_{i+1}",
-            dist.Binomial(total_count=m.total_goals(), probs=prob_A),
-            obs=m.goals_A(),
+            name=f"match_{i+1}",
+            fn=dist.Binomial(total_count=m.goals_a + m.goals_b, probs=prob_A),
+            obs=torch.tensor(m.goals_a),
         )
 
-    def guide(data):
+    """
+    def guide(data: CalcettoData):
         # register the two variational parameters with Pyro.
-        alpha_q = pyro.param(
-            "alpha_q", torch.tensor(15.0), constraint=constraints.positive
-        )
-        beta_q = pyro.param(
-            "beta_q", torch.tensor(15.0), constraint=constraints.positive
+        l = len(data.players())
+        mean_q = pyro.param("mean_q", torch.zeros(l))
+        cov_q = pyro.param(
+            "cov_q", torch.eye(l), constraint=constraints
         )
         # sample latent_fairness from the distribution Beta(alpha_q, beta_q)
         pyro.sample("latent_fairness", dist.Beta(alpha_q, beta_q))
+        
+        dist.MultivariateNormal()
+    """
+
+
+data = CalcettoData("dataset/log.csv")
+
+guide = AutoMultivariateNormal(model=model)
+n_steps = 10000
+
+
+# setup the optimizer
+adam_params = {"lr": 0.001}
+optimizer = Adam(adam_params)
+
+# setup the inference algorithm
+svi = SVI(model, guide, optimizer, loss=Trace_ELBO(vectorize_particles=True, num_particles=3))
+
+losses = np.zeros(n_steps)
+
+# do gradient steps
+for i in range(n_steps):
+    losses[i] = svi.step(data)
+    if i%100 == 0:
+        plt.plot(losses[:i])
+        plt.savefig('loss.png')
+        plt.close()
+
+        mean = pyro.get_param_store()['AutoMultivariateNormal.loc']
+        std = pyro.get_param_store()['AutoMultivariateNormal.scale']
+        corr = pyro.get_param_store()['AutoMultivariateNormal.scale_tril']
+
+        stats = {p : {'median': np.exp2(mean[i].item()), 'std': std[i].item()} for i, p in enumerate(data.get_players())}
+
+        print('stats: ')
+        print(json.dumps(stats, indent=4))
+
+        print(f'{corr=}: ')
+
+        players = np.array(data.get_players())
+        medians = np.exp2(mean.detach().numpy())
+        order = np.argsort(-medians)
+        y_pos = np.arange(len(mean))
+        
+        plt.barh(y = y_pos, width=medians[order])
+        plt.yticks(y_pos, labels=players[order])
+        plt.savefig('stats.png')
+        plt.close()
