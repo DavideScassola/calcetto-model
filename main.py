89a0c4c7d25e0d457ed8dd94f035ad4e0df10145
Davide Scassola <davide.scassola1@gmail.com>
2023-02-26 23:27:35 +0100

    distrinution plots

--- main.py
+++ main.py
@@ -5,17 +5,45 @@ import numpy as np
 import pandas as pd
 import pyro
 import seaborn as sns
+import torch
 from pyro.infer import SVI, Trace_ELBO
 from pyro.infer.autoguide.guides import AutoMultivariateNormal
 from pyro.optim import SGD, Adam
-from scipy.stats import norm
-from tqdm import tqdm
+from scipy.stats import lognorm, norm
 
 from calcetto_data import CalcettoData
 from calcetto_model import model
 
+
+def LogNormalMarginalPlots(*, loc: torch.Tensor, scale: torch.Tensor, players):
+    loc = loc.detach().numpy()
+    scale = scale.detach().numpy()
+
+    l = len(loc)
+    cols = 4
+    rows = int(np.ceil(l / cols))
+    fig, axes = plt.subplots(rows, cols, sharey=True)
+    fig.tight_layout()
+    fig.set_size_inches(12, 12)
+
+    for i in range(l):
+        ax = axes[i // cols, i % cols]
+
+        d = lognorm(scale[i])
+        alpha = 0.01
+        min_x = d.ppf(alpha)
+        max_x = d.ppf(1 - alpha)
+        x = np.linspace(min_x, max_x, 200)
+        ax.plot(x * np.exp(loc[i]), d.pdf(x=x))
+        ax.set_title(players[i])
+        # ax.set_xlim(50, 100)
+
+    plt.savefig("distributions.png")
+    plt.close()
+
+
 if __name__ == "__main__":
-    data = CalcettoData("dataset/log.csv")
+    data = CalcettoData("dataset/log2.csv")
 
     guide = AutoMultivariateNormal(model=model)
 
@@ -53,11 +81,11 @@ if __name__ == "__main__":
             k_std = pyro.get_param_store()["AutoMultivariateNormal.scale"][-1]
 
             stats = {
-                p: {"median": np.exp2(mean[i].item()), "std": std[i].item()}
+                p: {"median": np.exp(mean[i].item()), "std": std[i].item()}
                 for i, p in enumerate(data.get_players())
             }
 
-            stats["k"] = {"median": np.exp2(k_mean.item()), "std": k_std.item()}
+            stats["k"] = {"median": np.exp(k_mean.item()), "std": k_std.item()}
 
             print("stats: ")
             print(json.dumps(stats, indent=4))
@@ -65,9 +93,9 @@ if __name__ == "__main__":
             print(f"{corr=}: ")
 
             players = np.array(data.get_players())
-            medians = np.exp2(mean.detach().numpy())
-            quantiles_05 = np.exp2(norm(mean.detach(), std.detach()).ppf(0.05))
-            quantiles_95 = np.exp2(norm(mean.detach(), std.detach()).ppf(0.95))
+            medians = np.exp(mean.detach().numpy())
+            quantiles_05 = np.exp(norm(mean.detach(), std.detach()).ppf(0.05))
+            quantiles_95 = np.exp(norm(mean.detach(), std.detach()).ppf(0.95))
 
             # aestetic_rescale = 80 / np.max(medians)
             aestetic_rescale = 1
@@ -89,10 +117,13 @@ if __name__ == "__main__":
             plt.yticks(y_pos, labels=players[order])
             plt.xlim(np.min(quantiles_05) / 1.01, np.max(quantiles_95) * 1.01)
             plt.grid(alpha=0.8)
+            plt.gca().xaxis.set_major_locator(plt.MultipleLocator(5))
             plt.savefig("stats.png")
             plt.close()
 
             if i % 200 == 0:
+                LogNormalMarginalPlots(loc=mean, scale=std, players=players)
+
                 corr_for_plot = np.round(corr.detach().numpy(), 2)
                 corr_for_plot += corr_for_plot.T
                 np.fill_diagonal(corr_for_plot, np.nan)
